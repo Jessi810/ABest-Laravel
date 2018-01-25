53d174effd38bff90bb6e087295bcb378ad95aa1
Jessi810 <jessisibayan@gmail.com>
2018-01-25 15:00:05 +0800

    Add layout file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,97 @@
-@extends('layouts.app')
+@extends('layouts.universal', ['title' => 'Home'])
+
+@section('head')
+    <!-- owl carousel css -->
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/owl.theme.css') }}">
+@endsection
+
+@section('script')
+    <!-- owl carousel -->
+    <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <div class="container-fluid">
+        <div class="row">
+            <section>
+                <!-- *** HOMEPAGE CAROUSEL *** _________________________________________________________ -->
+                <div class="home-carousel">
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+                    <div class="dark-mask"></div>
+
+                    <div class="container">
+                        <div class="homepage owl-carousel">
+                            <div class="item">
+                                <div class="row">
+                                    <div class="col-sm-5 right">
+                                        <p>
+                                            <img src="img/logo.png" alt="">
+                                        </p>
+                                        <h1>Multipurpose responsive theme</h1>
+                                        <p>Business. Corporate. Agency.
+                                            <br />Portfolio. Blog. E-commerce.</p>
+                                    </div>
+                                    <div class="col-sm-7">
+                                        <img class="img-responsive" src="img/template-homepage.png" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="row">
+
+                                    <div class="col-sm-7 text-center">
+                                        <img class="img-responsive" src="img/template-mac.png" alt="">
+                                    </div>
+
+                                    <div class="col-sm-5">
+                                        <h2>46 HTML pages full of features</h2>
+                                        <ul class="list-style-none">
+                                            <li>Sliders and carousels</li>
+                                            <li>4 Header variations</li>
+                                            <li>Google maps, Forms, Megamenu, CSS3 Animations and much more</li>
+                                            <li>+ 11 extra pages showing template features</li>
+                                        </ul>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="row">
+                                    <div class="col-sm-5 right">
+                                        <h1>Design</h1>
+                                        <ul class="list-style-none">
+                                            <li>Clean and elegant design</li>
+                                            <li>Full width and boxed mode</li>
+                                            <li>Easily readable Roboto font and awesome icons</li>
+                                            <li>7 preprepared colour variations</li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-sm-7">
+                                        <img class="img-responsive" src="img/template-easy-customize.png" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="row">
+                                    <div class="col-sm-7">
+                                        <img class="img-responsive" src="img/template-easy-code.png" alt="">
+                                    </div>
+                                    <div class="col-sm-5">
+                                        <h1>Easy to customize</h1>
+                                        <ul class="list-style-none">
+                                            <li>7 preprepared colour variations.</li>
+                                            <li>Easily to change fonts</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    @endif
-
-                    You are logged in!
+                        <!-- /.project owl-slider -->
+                    </div>
                 </div>
-            </div>
+                <!-- *** HOMEPAGE CAROUSEL END *** -->
+            </section>
         </div>
     </div>
-</div>
 @endsection
