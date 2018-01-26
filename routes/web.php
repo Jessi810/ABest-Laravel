9f2b24f0f2ac9653e5d68f6bdc123a91bc41db1c
Jessi810 <jessisibayan@gmail.com>
2018-01-26 08:36:41 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('transport', 'TransportController')->middleware('auth');
+Route::resource('package', 'PackageController')->middleware('auth');
