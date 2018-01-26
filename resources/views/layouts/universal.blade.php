df972168fa2773635c7b012af5b2280f07442a6a
Jessi810 <jessisibayan@gmail.com>
2018-01-26 09:37:03 +0800

    Update layout

--- resources/views/layouts/universal.blade.php
+++ resources/views/layouts/universal.blade.php
@@ -405,23 +405,25 @@
 
             </header>
 
-            <div id="heading-breadcrumbs">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-7">
-                            <h1>{{ $heading_title or '' }}</h1>
+            @if(!empty($heading_title))
+                <div id="heading-breadcrumbs">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-7">
+                                <h1>{{ $heading_title or '' }}</h1>
+                            </div>
+                            {{--  <div class="col-md-5">
+                                <ul class="breadcrumb">
+                                    <li><a href="index.html">Home</a>
+                                    </li>
+                                    <li>Category full</li>
+                                </ul>
+        
+                            </div>  --}}
                         </div>
-                        {{--  <div class="col-md-5">
-                            <ul class="breadcrumb">
-                                <li><a href="index.html">Home</a>
-                                </li>
-                                <li>Category full</li>
-                            </ul>
-    
-                        </div>  --}}
                     </div>
                 </div>
-            </div>
+            @endif
 
             @yield('content')
 
