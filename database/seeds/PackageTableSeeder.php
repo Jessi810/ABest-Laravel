c605cdf2aa1bb729f4e1b7bef52a963777e1b223
Jessi810 <jessisibayan@gmail.com>
2018-01-26 10:37:46 +0800

    Update seeder (package)

--- database/seeds/PackageTableSeeder.php
+++ database/seeds/PackageTableSeeder.php
@@ -19,5 +19,11 @@ class PackageTableSeeder extends Seeder
         $package->price_1 = 150;
         $package->price_2 = 300;
         $package->transport()->associate(Transport::find(1))->save();
+
+        $package = new Package();
+        $package->size = 6;
+        $package->price_1 = 100;
+        $package->price_2 = 450;
+        $package->transport()->associate(Transport::find(1))->save();
     }
 }
