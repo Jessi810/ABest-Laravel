5efabe35eda464aece1e4122d0889428fe16f385
Jessi810 <jessisibayan@gmail.com>
2018-02-06 07:50:16 +0800

    Add Plupload asset files

--- resources/views/layouts/universal.blade.php
+++ resources/views/layouts/universal.blade.php
@@ -57,7 +57,7 @@
                     <div class="container">
                         <div class="row">
                             <div class="col-xs-5 contact">
-                                <p class="hidden-sm hidden-xs">Contact us on +420 777 555 333 or hello@universal.com.</p>
+                                <p class="hidden-sm hidden-xs">Contact us on <font color="#f48342">+1 587 500 0888</font> or <a href="mailto:info@abesttransport.com" style="color: #f48342;">info@abesttransport.com</font></p>
                                 <p class="hidden-md hidden-lg"><a href="#" data-animate-hover="pulse"><i class="fa fa-phone"></i></a>  <a href="#" data-animate-hover="pulse"><i class="fa fa-envelope"></i></a>
                                 </p>
                             </div>
@@ -70,8 +70,12 @@
                                 </div>
 
                                 <div class="login">
-                                    <a href="#" data-toggle="modal" data-target="#login-modal"><i class="fa fa-sign-in"></i> <span class="hidden-xs text-uppercase">Sign in</span></a>
-                                    <a href="customer-register.html"><i class="fa fa-user"></i> <span class="hidden-xs text-uppercase">Sign up</span></a>
+                                    @if (Auth::check())
+                                        
+                                    @else
+                                        <a href="{{ route('login') }}"><i class="fa fa-sign-in"></i> <span class="hidden-xs text-uppercase">Sign in</span></a>
+                                        <a href="{{ route('register') }}"><i class="fa fa-user"></i> <span class="hidden-xs text-uppercase">Sign up</span></a>
+                                    @endif
                                 </div>
 
                             </div>
