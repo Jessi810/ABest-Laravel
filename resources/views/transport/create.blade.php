c97c26e2ae03b38298188e5a443959b5fc5ce2c8
Jessi810 <jessisibayan@gmail.com>
2018-02-09 09:35:48 +0800

    Update validation failed message in create package page

--- resources/views/transport/create.blade.php
+++ resources/views/transport/create.blade.php
@@ -19,12 +19,14 @@
     <div id="content">
         <div class="container">
 
-            @if (count($errors) > 0)
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
             @endif
             
             <form action="{{ route('transport.store') }}" method="post" enctype="multipart/form-data">
@@ -34,51 +36,51 @@
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="name">Name</label>
-                            <input type="text" class="form-control" name="name" id="name" placeholder="Name">
+                            <input type="text" class="form-control" name="name" id="name" placeholder="Name" value="{{ old('name') }}">
                         </div>
         
                         <div class="form-group">
                             <label for="source">Source</label>
-                            <input type="text" class="form-control" name="source" id="source" placeholder="Source location">
+                            <input type="text" class="form-control" name="source" id="source" placeholder="Source location" value="{{ old('source') }}">
                         </div>
         
                         <div class="form-group">
                             <label for="destination">Destination</label>
-                            <input type="text" class="form-control" name="destination" id="destination" placeholder="Destination location">
+                            <input type="text" class="form-control" name="destination" id="destination" placeholder="Destination location" value="{{ old('destination') }}">
                         </div>
                     </div>
 
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="size[0]">Size (SM)</label>
-                            <input type="text" class="form-control" name="size[0]" id="size[0]" placeholder="Size">
+                            <input type="text" class="form-control" name="size[0]" id="size[0]" placeholder="Size" value="{{ old('size.0') }}">
                         </div>
         
                         <div class="form-group">
                             <label for="price_1[0]">1-way Price</label>
-                            <input type="text" class="form-control" name="price_1[0]" id="price_1[0]" placeholder="Price 1-way">
+                            <input type="text" class="form-control" name="price_1[0]" id="price_1[0]" placeholder="Price 1-way" value="{{ old('price_1.0') }}">
                         </div>
         
                         <div class="form-group">
                             <label for="price_2[0]">2-way Price</label>
-                            <input type="text" class="form-control" name="price_2[0]" id="price_2[0]" placeholder="Price 2-way">
+                            <input type="text" class="form-control" name="price_2[0]" id="price_2[0]" placeholder="Price 2-way" value="{{ old('price_2.0') }}">
                         </div>
                     </div>
 
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="size[1]">Size (X/XL)</label>
-                            <input type="text" class="form-control" name="size[1]" id="size[1]" placeholder="Size">
+                            <input type="text" class="form-control" name="size[1]" id="size[1]" placeholder="Size" value="{{ old('size.1') }}">
                         </div>
                     
                         <div class="form-group">
                             <label for="price_1[1]">1-way Price</label>
-                            <input type="text" class="form-control" name="price_1[1]" id="price_1[1]" placeholder="Price 1-way">
+                            <input type="text" class="form-control" name="price_1[1]" id="price_1[1]" placeholder="Price 1-way" value="{{ old('price_1.1') }}">
                         </div>
                     
                         <div class="form-group">
                             <label for="price_2[1]">2-way Price</label>
-                            <input type="text" class="form-control" name="price_2[1]" id="price_2[1]" placeholder="Price 2-way">
+                            <input type="text" class="form-control" name="price_2[1]" id="price_2[1]" placeholder="Price 2-way" value="{{ old('price_2.1') }}">
                         </div>
                     </div>
                 </div>
