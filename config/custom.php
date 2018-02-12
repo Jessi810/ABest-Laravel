033a520b84209f55f2a24a12972ace6b01177d21
Jessi810 <jessisibayan@gmail.com>
2018-02-12 18:03:40 +0800

    Add custom.php config file

---
+++ config/custom.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Social Media External Links
+    |--------------------------------------------------------------------------
+    */
+    'facebook_ext' => '',
+    'gplus_ext' => '',
+    'twitter_ext' => '',
+
+    'telephone_number' => '+1 587 500 0888',
+    'email_support' => 'info@abesttransport.com',
+
+    'company_name' => 'ABest Transport and Tour Services Inc.',
+    'company_address' => '240 Skyview Ranch Rd NE Calgary, AB T3N 0P4',
+];
